5a473d6a9c125f670195a976e8dade546b6ac24f
Anthony Phan <anthony.phan.co@gmail.com>
2022-06-30 12:45:40 +0800

    removed unused packages

--- brew/install-packages.sh
+++ brew/install-packages.sh
@@ -3,10 +3,10 @@
 brew update                                 # Update homebrew
 
 # Apps
-brew install --cask bitwarden
+#brew install --cask bitwarden
 brew install --cask visual-studio-code
 brew install --cask google-chrome
-brew install --cask stats
+#brew install --cask stats
 brew install --cask rectangle
 brew install --cask drawio
 brew install --cask kap
@@ -15,26 +15,29 @@ brew install --cask microsoft-teams
 # Terminal
 brew install starship
 brew install --cask fig                     # Fig app needs to be run manually to finish setup
-brew install diff-so-fancy
+#brew install diff-so-fancy
 brew install direnv
-brew install navi                           # cheatsheets
+#brew install navi                           # cheatsheets
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
-brew install httpie
+#brew install httpie
 brew install shellcheck
-brew install git-town
+#brew install git-town
 
 # Dev
-brew install rustup-init; rustup-init -y
+#brew install rustup-init; rustup-init -y
 brew install fnm                            # Faster version of npm
 
 # Virtualization
-brew install podman
-brew install --cask xquartz                 # Macos X11 for X11 port forwarding
+#brew install podman
+#brew install --cask xquartz                 # Macos X11 for X11 port forwarding
 
 # Cloud
 brew install awscli
 brew install azure-cli
+brew tap hashicorp/tap
+brew install hashicorp/tap/terraform
+
 
 # Fonts
 brew tap homebrew/cask-fonts
