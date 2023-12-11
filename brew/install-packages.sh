aa5a230a87f5c12fbf1cb30e2155083f6a1ba72d
Anthony Phan <anthony.phan.co@gmail.com>
2023-12-11 14:44:35 +0800

    remove old software

--- brew/install-packages.sh
+++ brew/install-packages.sh
@@ -6,32 +6,32 @@ brew update                                 # Update homebrew
 # brew install --cask bitwarden
 brew install --cask visual-studio-code
 brew install --cask google-chrome
-brew install --cask rectangle
+# brew install --cask rectangle
 brew install --cask drawio
-brew install --cask kap
+# brew install --cask kap
 brew install --cask microsoft-teams
 brew install --cask notion
 brew install --cask messenger
-brew install --cask obsidian
+# brew install --cask obsidian
 brew install --cask spotify
-brew install --cask gimp
-brew install rustup
+# brew install --cask gimp
+# brew install rustup
 
 # Terminal
-brew install starship
-brew install --cask fig                     # Fig app needs to be run manually to finish setup
-brew install direnv
+# brew install starship
+# brew install --cask fig                     # Fig app needs to be run manually to finish setup
+# brew install direnv
 brew install zsh-autosuggestions
 brew install zsh-syntax-highlighting
 brew install shellcheck
-brew install --cask azure-data-studio
+# brew install --cask azure-data-studio
 brew install git-town
 
 # Dev
 #brew install rustup-init; rustup-init -y
 brew install fnm
 brew install --cask ngrok                   # Faster version of npm
-brew install --cask obsidian
+# brew install --cask obsidian
 
 # Virtualization
 # brew install podman
@@ -42,7 +42,7 @@ brew install docker-compose               # Macos X11 for X11 port forwarding
 # Cloud
 brew install awscli
 brew install azure-cli
-brew tap azure/bicep
+# brew tap azure/bicep
 brew install bicep
 brew tap hashicorp/tap
 brew install hashicorp/tap/terraform
@@ -50,5 +50,5 @@ brew tap azure/functions
 brew install azure-functions-core-tools@4
 
 # Fonts
-brew tap homebrew/cask-fonts
-brew install --cask font-fira-code-nerd-font
+# brew tap homebrew/cask-fonts
+#brew install --cask font-fira-code-nerd-font
